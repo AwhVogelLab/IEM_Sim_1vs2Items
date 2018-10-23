6b871a8fc69def5def8777bf8252a0935d8e1716
Joshua Foster <joshua.james.foster@gmail.com>
2018-10-23 17:01:41 -0500

    updates to runSimulation

--- runSimulation.m
+++ runSimulation.m
@@ -1,29 +1,48 @@
-%
 
-nSamps = 5000; % number of times to run the simulation
+% seed the random generator
+rng default % sets the generator to twister and sets the seed to 0 (as in matlab restarted)
+rng shuffle % generates a new seed based on the clock
+sim.rngSettings = rng; % save to p-struct
+
+nSamps = 10000; % number of times to run the simulation
 nSubsPerSamp = 28; 
-SS1_amp = 1;
-SS2_amp = 1;
-e_noise = 0;
-
+ss1_amp = 1;
+ss2_amp = 0.8;
+e_noise = 1;
 
 pval = nan(nSamps,1);
+pval_ttest = nan(nSamps,1); 
 mn_diff = nan(nSamps,1); 
-mn_SS1 = nan(nSamps,1);
-mn_SS2 = nan(nSamps,1); 
+sem_diff = nan(nSamps,1); 
+mn_ss1 = nan(nSamps,1);
+sem_ss1 = nan(nSamps,1);
+mn_ss2 = nan(nSamps,1); 
+sem_ss2 = nan(nSamps,1); 
+mn_ctf_ss1 = nan(nSamps,9);
+mn_ctf_ss2 = nan(nSamps,9); 
 
 for s = 1:nSamps
     fprintf('Sample = %d\n',s);
     tic
-    [pval(s) mn_diff(s) mn_ss1(s) mn_ss2(s)] = simulateSample(nSubsPerSamp,SS1_amp,SS2_amp,e_noise,0);
+    [pval(s) pval_ttest(s) mn_diff(s) sem_diff(s) mn_ss1(s) sem_ss1(s) mn_ss2(s) sem_ss2(s) mn_ctf_ss1(s,:) mn_ctf_ss2(s,:)] = simulateSample(nSubsPerSamp,ss1_amp,ss2_amp,e_noise,0);
     toc 
 end
 
-
+sim.nSamps = nSamps;
+sim.nSubsPerSamp = nSubsPerSamp; 
+sim.ss1_amp = ss1_amp;
+sim.ss2_amp = ss2_amp;
+sim.e_noise = e_noise;
 sim.pval = pval;
+sim.pval_ttest = pval_ttest; 
 sim.mn_diff = mn_diff;
+sim.sem_diff = sem_diff;
 sim.mn_ss1 = mn_ss1;
+sim.sem_ss1 = sem_ss1;
 sim.mn_ss2 = mn_ss2; 
+sim.sem_ss2 = sem_ss2; 
+sim.mn_ctf_ss1 = mn_ctf_ss1;
+sim.mn_ctf_ss2 = mn_ctf_ss2;
 save('Sim.mat','sim')
 
 
